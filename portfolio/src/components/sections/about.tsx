cb3a13913648a4e5866006d209cc92cd1137f951
IsmailElhassouni <ismailelhassouni@gmail.com>
2025-03-29 02:09:58 +0000

    try to remove the # in url after using href overfell everything semmes to be done

--- portfolio/src/components/sections/about.tsx
+++ portfolio/src/components/sections/about.tsx
@@ -1,91 +1,109 @@
-export const About =()=>{
-const frontendSkills = ["react", "typescript", "javascript", "html", "css", "tailwindcss", "bootstrap"];
-const backendSkills = ["nodejs", "express", "laravel", "mongodb", "mysql", "postgresql", "python"];
-const lowlevelSkills = ["c", "c++"];
+import { Onscroll } from "../Onscroll";
+export const About = () => {
+    const frontendSkills = ["react", "typescript", "javascript", "html", "css", "tailwindcss", "bootstrap"];
+    const backendSkills = ["nodejs", "express", "laravel", "mongodb", "mysql", "postgresql", "python"];
+    const lowlevelSkills = ["c", "c++"];
+
     return (
-        <section id="about" 
-        className="min-h-screen flex items-center  justify-center py-20">
+        <section
+            id="about"
+            className="min-h-screen flex items-center justify-center py-20"
+        >
+            <Onscroll>
+                
             <div className="max-w-3xl mx-auto px-4">
                 <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
                     About Me
                 </h2>
-        <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
-            <p className="test-grey-300 mb-6 ">
-                Passionate about technology and innovation, I am a full-stack developer with a <br/>
-                background in computer science. I have experience in developing web applicationsusing <br/>
-                modern technologies such as React, Node.js, Laravel ,and MongoDB. <br/>
-                and low level programming languages such as C and C++.<br/>
-                I am always eager to learn new things and improve my skills. I am a team player and I enjoy working 
-                with others to achieve common goals.<br/> I am also a quick learner and I am able to adapt to new environments<br/> and technologies quickly. 
-                I am looking for opportunities to work on challenging projects that will <br/>
-                allow me to grow as a developer and contribute to the success of the team.
-            </p>
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 ">
-                <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                    <h3 className="text-xl font-bold mb-4">Frontend</h3>
-                    <div className="flex flex-wrap gap-2">
-                        {frontendSkills.map((skill, index) => (
-                            <span key={index} className="bg-blue-500/20 hover:bg-blue-500/40 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition text-blue-500 px-3 py-1 rounded-full ">
-                                {skill}
-                            </span>
-                        ))}
+                <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+                    <p className="test-grey-300 mb-6">
+                        Passionate about technology and innovation, I am a full-stack developer with a <br />
+                        background in computer science. I have experience in developing web applications using <br />
+                        modern technologies such as React, Node.js, Laravel, and MongoDB. <br />
+                        and low-level programming languages such as C and C++. <br />
+                        I am always eager to learn new things and improve my skills. I am a team player and I enjoy working
+                        with others to achieve common goals. <br /> I am also a quick learner and I am able to adapt to new environments <br /> and technologies quickly.
+                        I am looking for opportunities to work on challenging projects that will <br />
+                        allow me to grow as a developer and contribute to the success of the team.
+                    </p>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                        <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                            <h3 className="text-xl font-bold mb-4">Frontend</h3>
+                            <div className="flex flex-wrap gap-2">
+                                {frontendSkills.map((skill, index) => (
+                                    <span
+                                        key={index}
+                                        className="bg-blue-500/20 hover:bg-blue-500/40 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition text-blue-500 px-3 py-1 rounded-full"
+                                    >
+                                        {skill}
+                                    </span>
+                                ))}
+                            </div>
+                        </div>
+                        <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                            <h3 className="text-xl font-bold mb-4">Backend</h3>
+                            <div className="flex flex-wrap gap-2">
+                                {backendSkills.map((skill, index) => (
+                                    <span
+                                        key={index}
+                                        className="bg-blue-500/20 hover:bg-blue-500/40 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition text-blue-500 px-3 py-1 rounded-full"
+                                    >
+                                        {skill}
+                                    </span>
+                                ))}
+                            </div>
+                        </div>
+                        <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                            <h3 className="text-xl font-bold mb-4">System Development</h3>
+                            <div className="flex flex-wrap gap-2">
+                                {lowlevelSkills.map((skill, index) => (
+                                    <span
+                                        key={index}
+                                        className="bg-blue-500/20 hover:bg-blue-500/40 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition text-blue-500 px-3 py-1 rounded-full"
+                                    >
+                                        {skill}
+                                    </span>
+                                ))}
+                            </div>
+                        </div>
                     </div>
-            </div>
-            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                    <h3 className="text-xl font-bold mb-4">backend</h3>
-                    <div className="flex flex-wrap gap-2">
-                        {backendSkills.map((skill, index) => (
-                            <span key={index} className="bg-blue-500/20 hover:bg-blue-500/40 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition text-blue-500 px-3 py-1 rounded-full ">
-                                {skill}
-                            </span>
-                        ))}
-                    </div>
-            </div>
-            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                    <h3 className="text-xl font-bold mb-4">system development</h3>
-                    <div className="flex flex-wrap gap-2">
-                        {lowlevelSkills.map((skill, index) => (
-                            <span key={index} className="bg-blue-500/20 hover:bg-blue-500/40 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition text-blue-500 px-3 py-1 rounded-full ">
-                                {skill}
-                            </span>
-                        ))}
-                    </div>
-            </div>
-            </div>
+                </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
-            <div className="rounded-xl p-6 
-            border-white/10 border
-            hover:-translate-y-1 transition-all">
-                <h3 className ="text-xl font-bold mb-4"> <span className="mr-2">🎓</span>Educations</h3>
-                <ul className="list-disc list-inside text-gray-300 space-y-2">
-                    <li>
-                        <strong>Software Engineer </strong> - 1337 UM6P [2023-present]
-                       
-                    </li>
-                    <li>
-                        <strong>Full Stack </strong> - ISTA OFPPT [2021-2023]
-                       
-                    </li>
-                </ul>
-            </div>
-            <div className="rounded-xl p-6 
-            border-white/10 border
-            hover:-translate-y-1 transition-all">
-                <h3 className ="text-xl font-bold mb-4"> <span className="mr-2">💼</span>Work Experience</h3>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+                    <div
+                        className="rounded-xl p-6 border-white/10 border hover:-translate-y-1 transition-all"
+                    >
+                        <h3 className="text-xl font-bold mb-4">
+                            <span className="mr-2">🎓</span>Educations
+                        </h3>
+                        <ul className="list-disc list-inside text-gray-300 space-y-2">
+                            <li>
+                                <strong>Software Engineer</strong> - 1337 UM6P [2023-present]
+                            </li>
+                            <li>
+                                <strong>Full Stack</strong> - ISTA OFPPT [2021-2023]
+                            </li>
+                        </ul>
+                    </div>
+                    <div
+                        className="rounded-xl p-6 border-white/10 border hover:-translate-y-1 transition-all"
+                    >
+                        <h3 className="text-xl font-bold mb-4">
+                            <span className="mr-2">💼</span>Work Experience
+                        </h3>
                         <div className="space-y-4 text-gray-300">
                             <div>
-                                <h4 className="font-semibold">Full stack at ocp: intership (2023)</h4>
+                                <h4 className="font-semibold">Full Stack at OCP: Internship (2023)</h4>
                                 <p>
-                                    Assisted in building a web application for managing the OCP Group internship requests.<br/>
+                                    Assisted in building a web application for managing the OCP Group internship requests. <br />
                                     <strong>Technologies:</strong> Laravel, MySQL, Bootstrap, JavaScript.
                                 </p>
                             </div>
                         </div>
+                    </div>
+                </div>
             </div>
-        </div>
-        </div>
+            </Onscroll>
         </section>
     );
-}
+};
--- portfolio/src/components/sections/home.tsx
+++ portfolio/src/components/sections/home.tsx
@@ -1,7 +1,9 @@
+import { Onscroll } from "../Onscroll";
 export const Home = () => {
     return (
         <section id="home" 
         className="min-h-screen flex items-center justify-center relative ">
+        <Onscroll>
         <div className="text-center z-10 px-4">
                 <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-600 bg-clip-text text-transparent leading-right">
                     Hello, I'm Ismail Elhassouni
@@ -11,11 +13,15 @@ export const Home = () => {
 
             </p>
             <div className="flex justify-center space-x-4">
-                <a href="#Projects" className="bg-blue-500 text-white py-3 px-6 rounded font-mediun transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130, 246,0.4)]">View Projects</a>
-                <a href="#contact" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130, 246,0.4)] hover:bg-blue-500/10">View Projects</a>
-                
+                <a href="#Projects" 
+                className="bg-blue-500 text-white py-3 px-6 rounded font-mediun transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130, 246,0.4)]"
+                >View Projects</a>
+                <a href="#Contact" 
+                className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130, 246,0.4)] hover:bg-blue-500/10"
+                >Contact Me</a>    
             </div>
         </div> 
+        </Onscroll>
         </section>
     );
 }
